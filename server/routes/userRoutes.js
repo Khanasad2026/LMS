e6d93877b1ba42499ad9411d2b6f3bed9a9c9bda
Khanasad2026 <asadulla.20225014@mnnit.ac.in>
2025-04-21 18:19:45 +0530

    backend complete

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,4 +1,4 @@
-import { getUserData, purcahseCourse, userEnrolledCourses } from "../controllers/userController.js"
+import { addUserRating, getUserCourseProgress, getUserData, purcahseCourse, updateUserCourseProgress, userEnrolledCourses } from "../controllers/userController.js"
 import express from 'express'
 
 
@@ -7,5 +7,9 @@ const userRouter = express.Router()
 userRouter.get('/data', getUserData)
 userRouter.get('/enrolled-courses', userEnrolledCourses)
 userRouter.post('/purchase', purcahseCourse)
+
+userRouter.post('/update-course-progress',updateUserCourseProgress)
+userRouter.post('/get-course-progress',getUserCourseProgress)
+userRouter.post('/add-rating',addUserRating)
 
 export default userRouter;
