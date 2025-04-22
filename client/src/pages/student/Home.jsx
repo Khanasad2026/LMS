00815cc84154cf5ae452fe5cc6d83316940d2665
Khanasad2026 <asadulla.20225014@mnnit.ac.in>
2025-04-22 17:05:31 +0530

    Error corrected

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -3,7 +3,7 @@ import Hero from '../../components/student/Hero'
 import Companies from '../../components/student/Companies'
 import CoursesSection from '../../components/student/CoursesSection'
 import TestimonialsSection from '../../components/student/TestimonialsSection'
-import CallToAction from '../../components/student/CalltoAction'
+import CallToAction from '../../components/student/CallToAction'
 import Footer from '../../components/student/Footer'
 
 const Home = () => {
